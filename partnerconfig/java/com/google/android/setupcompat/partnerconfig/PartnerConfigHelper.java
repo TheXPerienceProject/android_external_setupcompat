1c377cb1f388904e0266177bc43093ab0adee2e0
Erfan Abdi <erfangplus@gmail.com>
2022-03-03 07:13:54 +0330

    Enable DayNight and dynamic colors by default
    Change-Id: I2ea5f8d07b579c013684d92c34d4b451bf7d816b

--- partnerconfig/java/com/google/android/setupcompat/partnerconfig/PartnerConfigHelper.java
+++ partnerconfig/java/com/google/android/setupcompat/partnerconfig/PartnerConfigHelper.java
@@ -726,9 +726,9 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "SetupWizard DayNight supporting status unknown; return as false.");
+        Log.w(TAG, "SetupWizard DayNight supporting status unknown; return as true.");
         suwDayNightEnabledBundle = null;
-        return false;
+        return true;
       }
     }
 
@@ -751,9 +751,9 @@ public class PartnerConfigHelper {
       } catch (IllegalArgumentException | SecurityException exception) {
         Log.w(
             TAG,
-            "SetupWizard extended partner configs supporting status unknown; return as false.");
+            "SetupWizard extended partner configs supporting status unknown; return as true.");
         applyExtendedPartnerConfigBundle = null;
-        return false;
+        return true;
       }
     }
 
@@ -836,9 +836,9 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "SetupWizard dynamic color supporting status unknown; return as false.");
+        Log.w(TAG, "SetupWizard dynamic color supporting status unknown; return as true.");
         applyDynamicColorBundle = null;
-        return false;
+        return true;
       }
     }
 
